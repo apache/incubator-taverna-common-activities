3598a1b31aafe3b97517055fd261f8bdb4b63bc6
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-01-19 16:56:48 +0000

    Added activity factory
    git-svn-id: https://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.activities/trunk/wsdl-activity@12218 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ src/main/java/net/sf/taverna/t2/activities/wsdl/WSDLActivityFactory.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (C) 2010 The University of Manchester   
+ * 
+ *  Modifications to the initial code base are copyright of their
+ *  respective authors, or their employers as appropriate.
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *    
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *    
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ ******************************************************************************/
+package net.sf.taverna.t2.activities.wsdl;
+
+import java.net.URI;
+
+import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityFactory;
+
+/**
+ * An {@link ActivityFactory} for creating <code>WSDLActivity</code>.
+ * 
+ * @author David Withers
+ */
+public class WSDLActivityFactory implements ActivityFactory {
+
+	@Override
+	public WSDLActivity createActivity() {
+		return new WSDLActivity();
+	}
+
+	@Override
+	public URI getActivityURI() {
+		return URI.create(WSDLActivity.URI);
+	}
+
+	@Override
+	public Object createActivityConfiguration() {
+		return new WSDLActivityConfigurationBean();
+	}
+
+}
