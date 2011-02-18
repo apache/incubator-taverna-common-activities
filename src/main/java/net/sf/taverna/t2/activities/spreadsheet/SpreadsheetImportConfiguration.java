7b0cced0aacd5945da53518e38bcdd893711501c
david@mygrid.org.uk <david@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-02-18 15:18:25 +0000

    Fixed configuration URI.
    git-svn-id: http://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.activities/trunk/spreadsheet-import-activity@12636 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/activities/spreadsheet/SpreadsheetImportConfiguration.java
+++ src/main/java/net/sf/taverna/t2/activities/spreadsheet/SpreadsheetImportConfiguration.java
@@ -31,7 +31,7 @@ import net.sf.taverna.t2.workflowmodel.processor.config.ConfigurationProperty;
  * 
  * @author David Withers
  */
-@ConfigurationBean(uri = SpreadsheetImportActivity.URI + "/configuration")
+@ConfigurationBean(uri = SpreadsheetImportActivity.URI + "#Config")
 public class SpreadsheetImportConfiguration {
 
 	private Range columnRange;
