bfd512a911bae953ada1784a41ed71ccc89537bc
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2011-06-27 15:09:45 +0000

    Catches when a correct username and password is not entered.
    git-svn-id: http://taverna.googlecode.com/svn/taverna/engine/net.sf.taverna.t2.activities/branches/maintenance/external-tool-activity@13353 bf327186-88b3-11dd-a302-d386e5130c1c

--- src/main/java/net/sf/taverna/t2/activities/externaltool/RetrieveLoginFromTaverna.java
+++ src/main/java/net/sf/taverna/t2/activities/externaltool/RetrieveLoginFromTaverna.java
@@ -6,6 +6,7 @@ import net.sf.taverna.t2.security.credentialmanager.CMException;
 import net.sf.taverna.t2.security.credentialmanager.CredentialManager;
 import net.sf.taverna.t2.security.credentialmanager.UsernamePassword;
 import de.uni_luebeck.inb.knowarc.usecases.invocation.AskUserForPw;
+import de.uni_luebeck.inb.knowarc.usecases.invocation.InvocationException;
 
 public class RetrieveLoginFromTaverna implements AskUserForPw {
 	private CredentialManager credentialManager = null;
@@ -31,8 +32,11 @@ public class RetrieveLoginFromTaverna implements AskUserForPw {
 		}
 	}
 
-	public String getUsername() {
+	public String getUsername() throws RuntimeException {
 		final UsernamePassword userPass = getUserPass();
+		if (userPass == null) {
+			throw new RuntimeException("Unable to obtain valid username and password");
+		}
 		userPass.resetPassword();
 		return userPass.getUsername();
 	}
