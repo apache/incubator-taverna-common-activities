8df57e9ad7c4b2c83699e5e42c58446d295ccd1f
alan@mygrid.org.uk <alan@mygrid.org.uk@bf327186-88b3-11dd-a302-d386e5130c1c>
2012-02-21 16:34:51 +0000

    Different types of interaction
    git-svn-id: http://taverna.googlecode.com/svn/unsorted/interaction-service/trunk/interaction-activity@14540 bf327186-88b3-11dd-a302-d386e5130c1c

---
+++ src/main/java/net/sf/taverna/t2/activities/interaction/InteractionActivityType.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package net.sf.taverna.t2.activities.interaction;
+
+/**
+ * @author alanrw
+ *
+ */
+public enum InteractionActivityType {
+	
+	VelocityTemplate,
+	RemotelyPresentededHtml,
+	LocallyPresentedHtml
+
+}
