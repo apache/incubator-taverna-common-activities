142667c2ea73ba09a2e50c9169ec7943e22dbd3e
Stian Soiland-Reyes <stain@apache.org>
2016-05-04 00:47:12 +0100

    TAVERNA-963 ASF license header
    > Hajo Krabbenhoeft and Steffen Moelller have both signed ICLAs for pre-Apache
    > Taverna. That licensed their work to Manchester University and should be
    > covered by the University's transfer to Apache.

--- taverna-external-tool-activity/src/main/java/de/uni_luebeck/inb/knowarc/gui/PleaseWaitDialog.java
+++ taverna-external-tool-activity/src/main/java/de/uni_luebeck/inb/knowarc/gui/PleaseWaitDialog.java
@@ -1,21 +1,19 @@
-/* Part of the KnowARC Janitor Use-case processor for taverna
- *  written 2007-2010 by Hajo Nils Krabbenhoeft and Steffen Moeller
- *  University of Luebeck, Institute for Neuro- and Bioinformatics
- *  University of Luebeck, Institute for Dermatolgy
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *  This package is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  This package is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this package; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package de.uni_luebeck.inb.knowarc.gui;
