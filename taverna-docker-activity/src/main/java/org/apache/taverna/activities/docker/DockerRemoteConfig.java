d4be680580f7509c57cc07bb6c3a4dcf3c1155b4
Nadeesh Dilanga <nadeesh092@gmail.com>
2016-06-24 01:51:04 -0400

    Adding basic docker command capability to docker activity plugin.

---
+++ taverna-docker-activity/src/main/java/org/apache/taverna/activities/docker/DockerRemoteConfig.java
@@ -0,0 +1,104 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements. See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership. The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+package org.apache.taverna.activities.docker;
+
+public class DockerRemoteConfig {
+
+    private String dockerHost;
+    private String registryUsername;
+    private String registryPassword;
+    private String registryEmail;
+    private String registryUrl;
+    private String dockerConfig;
+    private String dockerCertPath;
+    private boolean dockerTlsVerify;
+    private String apiVersion = "1.21";
+
+    public String getDockerHost() {
+        return dockerHost;
+    }
+
+    public void setDockerHost(String dockerHost) {
+        this.dockerHost = dockerHost;
+    }
+
+    public String getRegistryUsername() {
+        return registryUsername;
+    }
+
+    public void setRegistryUsername(String registryUsername) {
+        this.registryUsername = registryUsername;
+    }
+
+    public String getRegistryPassword() {
+        return registryPassword;
+    }
+
+    public void setRegistryPassword(String registryPassword) {
+        this.registryPassword = registryPassword;
+    }
+
+    public String getRegistryEmail() {
+        return registryEmail;
+    }
+
+    public void setRegistryEmail(String registryEmail) {
+        this.registryEmail = registryEmail;
+    }
+
+    public String getRegistryUrl() {
+        return registryUrl;
+    }
+
+    public void setRegistryUrl(String registryUrl) {
+        this.registryUrl = registryUrl;
+    }
+
+    public String getDockerConfig() {
+        return dockerConfig;
+    }
+
+    public void setDockerConfig(String dockerConfig) {
+        this.dockerConfig = dockerConfig;
+    }
+
+    public String getDockerCertPath() {
+        return dockerCertPath;
+    }
+
+    public void setDockerCertPath(String dockerCertPath) {
+        this.dockerCertPath = dockerCertPath;
+    }
+
+    public boolean isDockerTlsVerify() {
+        return dockerTlsVerify;
+    }
+
+    public void setDockerTlsVerify(boolean dockerTlsVerify) {
+        this.dockerTlsVerify = dockerTlsVerify;
+    }
+
+    public String getApiVersion() {
+        return apiVersion;
+    }
+
+    public void setApiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
+    }
+}
